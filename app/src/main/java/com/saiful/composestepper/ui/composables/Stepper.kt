07416ba17d256ac40ece05d77ea51c56b7937587
Saiful Islam <saifulsaif321@gmail.com>
2025-04-27 01:04:01 +0600

    stepper ui updated

--- app/src/main/java/com/saiful/composestepper/ui/composables/Stepper.kt
+++ app/src/main/java/com/saiful/composestepper/ui/composables/Stepper.kt
@@ -1,6 +1,5 @@
 package com.saiful.composestepper.ui.composables
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Done
@@ -11,23 +10,38 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
 
 @Composable
-fun Stepper() {
+fun Stepper(
+    numberOfSteps: Int,
+    activeStep: Int,
+    stepTitle: List<String>
+) {
     Row(
         modifier = Modifier
             .padding(12.dp),
     ) {
 
         Row {
-            repeat(times = 3) {
+            repeat(times = numberOfSteps) {
                 StepperItem(
-                    modifier = if (it != 2) Modifier.weight(1f) else Modifier,
-                    hasNextStep = it != 2
+                    modifier = Modifier.weight(1f),
+                    hasNextStep = it != numberOfSteps - 1,
+                    hasPrevious = it != 0,
+                    state = when {
+                        it == activeStep - 1 -> StepState.ACTIVE
+                        it > activeStep - 1 -> StepState.UPCOMING
+                        it < activeStep - 1 -> StepState.DONE
+                        else -> StepState.ACTIVE
+                    },
+                    stepTitle = stepTitle[it],
+                    stepValue = it + 1
                 )
             }
         }
@@ -41,36 +55,73 @@ fun Stepper() {
 private fun StepperItem(
     modifier: Modifier,
     hasNextStep: Boolean,
+    hasPrevious: Boolean,
+    state: StepState,
+    stepTitle: String,
+    stepValue: Int
 ) {
 
-    ConstraintLayout(modifier = modifier.padding(vertical = 4.dp, horizontal = 2.dp)) {
-        val (circle, line, text) = createRefs()
+    ConstraintLayout(modifier = modifier) {
+        val (circle, line1, line2, text) = createRefs()
+
+
+        if (hasPrevious) {
+            HorizontalDivider(
+                modifier = Modifier
+                    .constrainAs(line1) {
+                        top.linkTo(circle.top)
+                        bottom.linkTo(circle.bottom)
+                        start.linkTo(parent.start)
+                        end.linkTo(circle.start, margin = 4.dp)
+                        width = Dimension.fillToConstraints
+                    },
+                thickness = 4.dp,
+                color = when (state) {
+                    StepState.DONE -> Color.Blue
+                    StepState.ACTIVE -> Color.Blue
+                    StepState.UPCOMING -> Color.Gray
+                }
+            )
+        }
 
         Box(
             modifier = Modifier
                 .constrainAs(circle) {
                     top.linkTo(parent.top)
                     start.linkTo(parent.start)
+                    end.linkTo(parent.end)
                 }
-                .wrapContentSize()
+                .size(32.dp)
                 .drawBehind {
                     val canvasWidth = size.width
                     val canvasHeight = size.height
                     val circleRadius = (size.width / 2).toDp().toPx()
                     drawCircle(
-                        color = Color.Red,
+                        color = when (state) {
+                            StepState.ACTIVE -> Color.Blue
+                            StepState.DONE -> Color.Blue
+                            StepState.UPCOMING -> Color.Gray
+                        },
                         center = Offset(x = canvasWidth / 2, y = canvasHeight / 2),
                         radius = circleRadius
                     )
                 },
             contentAlignment = Alignment.Center
         ) {
-            Icon(
-                imageVector = Icons.Filled.Done,
-                contentDescription = "Add",
-                modifier = Modifier.size(24.dp),
-                tint = Color.White // Set the color of the icon
-            )
+            if (state == StepState.DONE) {
+                Icon(
+                    imageVector = Icons.Filled.Done,
+                    contentDescription = "Add",
+                    modifier = Modifier.size(24.dp),
+                    tint = Color.White // Set the color of the icon
+                )
+            } else {
+                Text(
+                    modifier = Modifier,
+                    text = stepValue.toString(),
+                    style = TextStyle(color = Color.White)
+                )
+            }
         }
 
         Text(
@@ -78,20 +129,28 @@ private fun StepperItem(
                 top.linkTo(circle.bottom, margin = 2.dp)
                 start.linkTo(circle.start)
                 bottom.linkTo(parent.bottom)
+                end.linkTo(circle.end)
             },
-            text = "item"
+            text = stepTitle,
+            overflow = TextOverflow.Ellipsis,
+            maxLines = 1,
         )
 
         if (hasNextStep) HorizontalDivider(
             modifier = Modifier
-                .constrainAs(line) {
+                .constrainAs(line2) {
                     top.linkTo(circle.top)
                     bottom.linkTo(circle.bottom)
                     start.linkTo(circle.end, margin = 4.dp)
                     end.linkTo(parent.end)
                     width = Dimension.fillToConstraints
-                }
-                .background(Color.Blue)
+                },
+            thickness = 4.dp,
+            color = when (state) {
+                StepState.DONE -> Color.Blue
+                StepState.ACTIVE -> Color.Gray
+                StepState.UPCOMING -> Color.Gray
+            }
         )
     }
 
@@ -103,12 +162,31 @@ private fun StepperItem(
 private fun StepperItemPreview() {
     StepperItem(
         modifier = Modifier,
-        hasNextStep = true
+        hasNextStep = true,
+        hasPrevious = false,
+        state = StepState.ACTIVE,
+        stepTitle = "step 1",
+        stepValue = 1
     )
 }
 
 @Preview(showBackground = true)
 @Composable
 private fun StepperPreview() {
-    Stepper()
+    Stepper(
+        numberOfSteps = 4,
+        activeStep = 2,
+        stepTitle = listOf(
+            "step 1",
+            "step 2",
+            "step 3",
+            "step 4"
+        )
+    )
+}
+
+private enum class StepState {
+    ACTIVE,
+    DONE,
+    UPCOMING
 }
